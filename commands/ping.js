d1c3f7b4c46aa69d9fb709697368663b1f082d97
unknown <boenguillaume@gmail.com>
2020-12-25 23:33:59 +0100

    ajout readme

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 module.exports = {
 	name: "ping",
 	description: "ping pong",
-	execute(message, args) {
+	async execute(message, args) {
 		message.reply('pong!');
 	},
 };
